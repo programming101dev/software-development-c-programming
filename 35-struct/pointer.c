8fa931bf058c9810a695e970fb24a3a9e72939e3
programming101dev <113087924+programming101dev@users.noreply.github.com>
2024-08-19 11:54:40 -0700

    removd files that should not be there, updated code

--- 35-struct/pointer.c
+++ 35-struct/pointer.c
@@ -11,14 +11,20 @@ struct person    // NOLINT(altera-struct-pack-align)
 int main(void)
 {
     struct person  pat;
-    struct person *p = &pat;
+    struct person *p;
 
-    p->name   = "Pat Doe";
-    p->age    = 42;
-    p->height = 5.9F;
-
-    printf("Name: %s, Age: %d, Height: %.1f\n", p->name, p->age, (double)p->height);
+    p          = &pat;
+    pat.name   = "Pat Doe";
+    pat.age    = 42;
+    pat.height = 5.9F;
     printf("Name: %s, Age: %d, Height: %.1f\n", pat.name, pat.age, (double)pat.height);
+    printf("Name: %s, Age: %d, Height: %.1f\n", p->name, p->age, (double)p->height);
+
+    p->name   = "Chris Doe";
+    p->age    = 24;
+    p->height = 6.2F;
+    printf("Name: %s, Age: %d, Height: %.1f\n", pat.name, pat.age, (double)pat.height);
+    printf("Name: %s, Age: %d, Height: %.1f\n", p->name, p->age, (double)p->height);
 
     return EXIT_SUCCESS;
 }
